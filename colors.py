18959c7465f766eb8b099cb9ada5e41297553d08
pietrofesar <pietrofesar@users.noreply.github.com>
2017-11-08 10:46:00 -0500

    Update colors.py

--- colors.py
+++ colors.py
@@ -5,19 +5,20 @@
 # Esc[attribute;foreColor;aftColorm
 
 # you can set constants
-G = '\033[38;5;70m' # green
-Y = '\033[38;5;11m' # yellow
-R = '\033[38;5;09m' # red
-RST = '\033[0m' # reset
+G = '\033[1;32m' # bold green
+Y = '\033[1;33m' # bold yellow
+R = '\033[31m' # red
+X = '\033[0m' # reset
 
 
 # base color options
 for each in range(30, 47):
     print('\033[{}mTest number is {}'.format(each, each))
-print(RST)
+print(X)
 
 # bold added to base colors
 for each in range(30, 47):
     print('\033[1;{}mTest number is {}'.format(each, each))
-print(RST)
+print(X)
 
+print('{}This is an example.{}'.format(G, X))
